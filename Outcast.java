7af3049baca802cb144b403d12dedbd881aef488
Neha Agarwal <nehavm1617@gmail.com>
2022-11-27 21:43:17 -0500

    Update Outcast.java

--- Outcast.java
+++ Outcast.java
@@ -11,23 +11,14 @@ public class Outcast {
 
     // Returns the outcast noun from nouns.
     public String outcast(String[] nouns) {
-        // set a variable outC to an empty String to keep track of the outcast.
         String outC = "";
-        // Set a variable maxDistance to 0 to keep track of the max distance.
         int maxDistance = 0;
-        // for all the ith nouns in the array nouns...
         for (String i : nouns) {
-            // Set a variable tempDistance to 0.
             int tempdistance = 0;
-            // for the jth noun in the array nouns...
             for (String j : nouns) {
-                // compute the shortest ancestral distance between the ith noun and the jth noun.
                 tempdistance += wordnet.distance(i, j);
-                // if the tempDistance turns out to be bigger than the current maxDistance, then
-                // update the value od the maxDistance.
                 if (tempdistance > maxDistance) {
                     maxDistance = tempdistance;
-                    // set outCast to ith noun.
                     outC = i;
                 }
 
